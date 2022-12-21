20dfe8401598564f3fff8b9120588ea73a8f585c
Ester <55720120+EsterMou@users.noreply.github.com>
2022-12-21 20:34:10 -0300

    commit

--- Script/script.js
+++ Script/script.js
@@ -31,3 +31,39 @@ function eachBoxes(type, boxes){
         })
     }
 }
+
+// scroll navegações
+
+let navBtn = $('.nav-item')
+
+let bannerSection = $('#mainSlider')
+let aboutSection = $('#about-area')
+let servicesSection = $('#services-area')
+let teamSection = $('#team-area')
+let portifolioSection = $('#portifolio-area')
+let contacSection = $('#contac-area')
+
+let scrolTo = "";
+
+$(navBtn).click(function(){
+    let btnId = $(this).attr('id')
+
+    if(btnId=='About-menu'){
+        scrolTo = aboutSection
+    }else if (btnId == 'services-menu'){
+        scrolTo = servicesSection
+    }else if (btnId == 'team-menu'){
+        scrolTo = teamSection
+    }else if (btnId == 'portfolio-menu'){
+        scrolTo = portifolioSection
+    }else if(btnId == 'contact-menu'){
+        scrolTo = contacSection
+    }else {
+        scrolTo = bannerSection
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrolTop: $(scrolTo).offset().top - 70
+    }, 1500)
+})
+
